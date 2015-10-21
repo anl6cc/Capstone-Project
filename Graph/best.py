228fd77aae119ef66ab985cd9ad56a37d963162c
Alexander Luu <anl6cc@virginia.edu>
2015-10-20 22:44:43 -0400

    graphs patient file on matlablib

--- Graph/best.py
+++ Graph/best.py
@@ -1,20 +1,32 @@
-# read in file
-with open('test.ddvh', 'r') as f:
-    r = f.read();
-    print(r);
-    lines = r.splitlines();
+import matplotlib.pyplot as plt;
 
-    # loop through each line
-    n = -1;
-    data = dict();
-    for i in range(len(lines)):
-    	if(i == 0):
-    		n = int(lines[i]);
-    	else:
-    		line = lines[i];
-    		parts = line.split(' ');
-    		print(parts);
-    		data[parts[0]] = parts[1];
-    print(data);
+# read in file
+with open('../Patient_12508.Plan_14.dvh.1abCompositeDoseDelivered.esophagus.ddvh', 'r') as f:
+	r = f.read();
+	# print(r);
+	lines = r.splitlines();
+
+	# loop through each line
+	n = -1;
+	data = dict();
+	x = [];
+	y = [];
+	for i in range(len(lines)):
+		if(i == 0):
+			n = int(lines[i]);
+		else:
+			line = lines[i];
+			parts = line.split(' ');
+			# print(parts);
+			data[float(parts[0])] = float(parts[1]);
+			x.append(float(parts[0]));
+			y.append(float(parts[1]));
+	values = list(data.values());
+	keys = list(data.keys());
+	keys.sort();
+	print(x);
+	print(y);
+	plt.plot(x, y);
+	plt.show();
 
 
