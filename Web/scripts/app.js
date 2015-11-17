0dd6c84f2cc46988d98301ea9d69c7eed2b9d0e9
Alexander Luu <anl6cc@virginia.edu>
2015-11-16 21:05:13 -0500

    graph patient data using vis.js networks

--- Web/scripts/app.js
+++ Web/scripts/app.js
@@ -1,58 +1,96 @@
-alert('sup');
-/*
-function initPPGgraph()
-{
-    $scope.container = document.getElementById('visualization');
-    $scope.dataset = new vis.DataSet();
+  // JSON and node work on Aluu's computer
+  //console.log(JSON.stringify(vis));
 
-    var options = {
-	height: '200px',
-	width: '100%',
-	moveable: false,
-	// how much further from the beginning the graph starts
-	start: vis.moment().add(-10, 'seconds'),
-	end: vis.moment(),
-	dataAxis: {
-	    visible: false
-	},
-	drawPoints: false,
-	shaded: false,
-	showMajorLabels: false,
-	showMinorLabels: false
-    };
-    $scope.graph2d = new vis.Graph2d($scope.container, $scope.dataset, options);
-}
+  function readTextFile(file)
+  {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                //document.getElementById("textfile").innerHTML = rawFile.responseText;
+                //console.log(rawFile.responseText);
+                var dataString = rawFile.responseText;
+                initialize(dataString);
+            }
+        }
+    }
+    rawFile.send(null);
+  }
 
-//add the red num point to the real time ppg plot
-function plotRedNums()
-{	 
-	renderStep();
-	addDataPoint();
-}
+  readTextFile("./patient_data/Patient_12508.Plan_14.dvh.1abCompositeDoseDelivered.esophagus.ddvh");
+  //readTextFile("./patient_data/test.txt");
 
-// move the window of the graph
-function renderStep() {
-    var now = vis.moment();
-    var range = $scope.graph2d.getWindow();
-    var interval = range.end - range.start;
-    //discrete setting
-    $scope.graph2d.setWindow(now - interval, now, {animation: false});
-}
+  // initialize the graph
+  function initialize(dataString)
+  {
+      // create an array with nodes
+  /*
+  var nodes = new vis.DataSet([
+    {id: 1, color: 'lime', x: 0, y: 0, allowedToMoveX: true},
+    {id: 2, label: 'Node 2', x: 100.5, y: 100},
+    {id: 3, label: 'Node 3', x: 200, y: 200},
+    {id: 4, label: 'Node 4', x: 300, y: 300},
+    {id: 5, label: 'Node 5', x: 400, y: 400}
+  ]);
+  */
 
-// add a datapoint to the dataset and remove old datapoints
-function addDataPoint() {
-    var now = vis.moment();
-    $scope.dataset.add({
-      x: now,
-      y: $scope.redNums[$scope.redNums.length - 1]
-    });
-    // remove all data points which are no longer visible
-    var range = $scope.graph2d.getWindow();
-    var interval = range.end - range.start;
-    var oldIds = $scope.dataset.getIds({
-		filter: function (item) {
-			return item.x < range.start - interval;
-		}
-    });
-    $scope.dataset.remove(oldIds);
-}*/
+  var nodeArray = [];
+  var edgeArray = [];
+
+  var lines = dataString.split("\n");
+  var num = parseInt(lines[0], 10);
+
+  //console.log(JSON.stringify(lines));
+  
+  for(var i=1; i < num + 1; i++)
+  {
+    var pos = lines[i].split(" ");
+    nodeArray.push({id: i, x: parseFloat(pos[0]), y: parseFloat(pos[1]) * 10000});
+    if(i !== 1)
+    {
+      edgeArray.push({from: i - 1, to: i, length: 10});
+    }
+  }
+
+  console.log(JSON.stringify(nodeArray));
+  
+  var nodes = new vis.DataSet(nodeArray);
+  
+  // create an array with edges
+  /*
+  var edges = new vis.DataSet([
+    {from: 1, to: 2, length: 10},
+    {from: 2, to: 3, length: 10},
+    {from: 3, to: 4, length: 10},
+    {from: 4, to: 5, length: 10}
+  ]);
+  */
+  var edges = new vis.DataSet(edgeArray);
+
+  // create a network
+  var container = document.getElementById('mynetwork');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {
+    nodes: {
+        fixed: {
+            x: true,
+            y: true
+        }
+    },
+    edges: {
+        smooth: {
+            enabled: false
+        }, 
+        width: 5
+    },
+    height: '100%'
+  };
+  var network = new vis.Network(container, data, options);
+  }
